fd492ad2f205e36cdaeddbb10977d1c2f8ea3ebe
Ray McDermott <ray.mcdermott@external.toyota-europe.com>
2015-10-22 17:27:01 +0200

    :penguin: checking out how the IP works on the Space

--- vendor/datomic/start-datomic.sh
+++ vendor/datomic/start-datomic.sh
@@ -17,6 +17,16 @@ sed "s/^port=4334/port=$PORT/" ${PROPERTIES} > ${DYNO_PROPERTIES}
 
 unset JAVA_OPTS
 
+########------- START ---##### publish the dyno IP
+set -x
+ip -4 -o addr show dev eth1
+
+dyno-ip=$(ip -4 -o addr show dev eth1 | awk '{print $4}' | cut -d/ -f1)
+
+echo "DYNO_IP = $dyno-ip"
+set +x
+########------- END -----##### publish the dyno IP
+
 transactor -Xmx512m -Xms256m ${DYNO_PROPERTIES} | sed 's/\(.*\)&password=.*&\(.*\)/\1\&password=*****\&\2/'
 
 
