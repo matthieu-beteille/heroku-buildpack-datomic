131955524d4ce8ded644cb5ca54c441ffa4e8a09
Ray McDermott <ray.mcdermott@toyota-europe.com>
2015-11-06 22:04:21 +0100

    :bug: do not expose transactor port to the Internet! Use the standard port to test that Space clients can connect using that port

--- vendor/datomic/start-datomic.sh
+++ vendor/datomic/start-datomic.sh
@@ -16,14 +16,10 @@ DYNO_PROPERTIES=${PROPERTIES}.heroku
 
 DYNO_IP=$(ip -4 -o addr show dev eth1 | awk '{print $4}' | cut -d/ -f1)
 
-sed -e "s/^host=localhost/host=${DYNO_IP}/" \
-    -e "s/^port=4334/port=${PORT}/"         \
-    ${PROPERTIES} > ${DYNO_PROPERTIES}
+sed "s/^host=localhost/host=${DYNO_IP}/" ${PROPERTIES} > ${DYNO_PROPERTIES}
 
 unset JAVA_OPTS
 
-
-
-# Do not log passwords (Datomic should not do this)
+# Do not log passwords
 
 transactor -Ddatomic.printConnectionInfo=false -Xmx2g -Xms256m ${DYNO_PROPERTIES}
