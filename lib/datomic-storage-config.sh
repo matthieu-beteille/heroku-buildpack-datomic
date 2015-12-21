f0aa29342a049aa78680c7a7d3575ab5967d6474
Ray McDermott <ray.mcdermott@toyota-europe.com>
2015-12-21 17:50:19 +0100

    :mag: tracing

--- lib/datomic-storage-config.sh
+++ lib/datomic-storage-config.sh
@@ -28,9 +28,14 @@ configure_ddb() {
 
     echo -n "-----> Configuring Datomic to connect to DynamoDB... "
 
+set -x
+
     SAMPLE_PROPERTIES_FILE=${BUILD_DIR}/datomic/config/samples/ddb-transactor-template.properties
     COPY_NAME=${PWD}/ddb-transactor-template-COPY.properties
     PATCHED_FILE_NAME=${PWD}/ddb-transactor-template-PATCHED.properties
+
+echo before copy pwd says
+pwd
 
     cp ${SAMPLE_PROPERTIES_FILE} ${COPY_NAME}
 
